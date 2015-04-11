2336cf1561b5a895d21f2e8c84a341ca1f3abe31
Oleksii Fedorov <waterlink000@gmail.com>
2015-04-12 01:40:52 +0300

    Include library.template by default

--- script/install.sh
+++ script/install.sh
@@ -8,7 +8,7 @@ echo 'deps { github "generate-cr/generate" }' > Projectfile
 
 crystal deps
 
-touch Generatefile
+echo 'template "library.template/generate/library", "generate-cr/library.template"' > Generatefile
 crystal build libs/generate/core.cr -o bin/generate-core
 
 curl https://raw.githubusercontent.com/generate-cr/generate/master/script/script.sh > bin/generate-cr
